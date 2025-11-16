fc9f94e56b550ab7c5e885f5cff9224f38f704d0
Muhammad <mohamedkam000@gmail.com>
2025-11-16 19:18:00 +0000

    sync

---
+++ app/build.gradle.kts
@@ -0,0 +1,108 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.plugin.compose")
+    id("org.jetbrains.kotlin.plugin.serialization")
+    id("com.google.dagger.hilt.android")
+    id("com.google.devtools.ksp")
+}
+
+android {
+    namespace = "com.unisource.app"
+    compileSdk = 36
+
+    defaultConfig {
+        applicationId = "com.unisource.app"
+        minSdk = 31
+        targetSdk = 36
+        versionCode = 1
+        versionName = "1.0"
+        ndk {
+            abiFilters += listOf("arm64-v8a")
+        }
+
+        androidResources {
+            localeFilters += setOf("en")
+        }
+    }
+
+    signingConfigs {
+        create("release") {
+            storeFile = file("us.p12")
+            storePassword = "1234"
+            keyAlias = "US"
+            keyPassword = "1234"
+            storeType = "pkcs12"
+        }
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = true
+            isShrinkResources = true
+//            proguardFiles(
+//                getDefaultProguardFile("proguard-android-optimize.txt"),
+//                "proguard-rules.pro"
+//            )
+            signingConfig = signingConfigs.getByName("release")
+        }
+    }
+
+    buildFeatures {
+        compose = true
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_25
+        targetCompatibility = JavaVersion.VERSION_25
+    }
+
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
+}
+
+dependencies {
+    implementation(platform("androidx.compose:compose-bom:2025.11.00"))
+    implementation("androidx.compose.ui:ui:1.10.0-beta01")
+    implementation("androidx.compose.animation:animation:1.9.0-rc01")
+    implementation("androidx.compose.ui:ui-tooling:1.10.0-alpha01")
+    implementation("androidx.compose.ui:ui-tooling-preview:1.10.0-beta01")
+    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.activity:activity-compose:1.12.0-alpha06")
+    implementation("androidx.compose.material:material-icons-core:1.7.8")
+    implementation("androidx.compose.runtime:runtime:1.10.0-alpha01")
+    implementation("androidx.compose.material3:material3:1.5.0-alpha02")
+    implementation("androidx.compose.foundation:foundation:1.10.0-beta01")
+    implementation("androidx.navigation:navigation-compose:2.9.3")
+    implementation("com.google.android.material:material:1.14.0-alpha04")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.9.0")
+    implementation("com.google.dagger:hilt-android:2.57.2")
+    ksp("com.google.dagger:hilt-compiler:2.57.2")
+    implementation("androidx.hilt:hilt-navigation-compose:1.3.0")
+    implementation("io.ktor:ktor-client-core:3.3.2")
+    implementation("io.ktor:ktor-client-android:3.3.2")
+    implementation("io.ktor:ktor-client-cio:3.3.2")
+    implementation("io.ktor:ktor-client-content-negotiation:3.3.2")
+    implementation("io.ktor:ktor-serialization-kotlinx-json:3.3.2")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.10.0-alpha02")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.10.0-alpha02")
+    implementation("androidx.room:room-runtime:2.8.2")
+    implementation("androidx.room:room-ktx:2.8.2")
+    implementation("androidx.room:room-common-jvm:2.8.2")
+    ksp("androidx.room:room-compiler:2.8.2")
+    implementation("com.squareup.okhttp3:okhttp:5.3.0")
+    implementation("com.squareup.retrofit2:retrofit:3.0.0")
+    implementation("com.squareup.retrofit2:converter-moshi:3.0.0")
+    implementation("com.squareup.retrofit2:converter-kotlinx-serialization:3.0.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.9.0")
+    implementation("com.squareup.okhttp3:okhttp:5.3.0")
+    implementation("com.squareup.moshi:moshi:1.15.2")
+    implementation("com.jakewharton.retrofit:retrofit2-kotlinx-serialization-converter:1.0.0")
+    implementation("io.coil-kt:coil-compose:2.7.0")
+    implementation("androidx.compose.animation:animation:1.10.0-beta01")
+
+    implementation("io.coil-kt.coil3:coil-compose:3.3.0")
+    implementation("io.coil-kt.coil3:coil-network-okhttp:3.3.0")
+}
---
+++ build.gradle.kts
@@ -0,0 +1,9 @@
+plugins {
+    id("com.android.application") version "9.0.0-alpha14" apply false
+    id("org.jetbrains.kotlin.android") version "2.3.0-Beta2" apply false
+    id("com.android.library") version "9.0.0-alpha14" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.3.0-Beta2" apply false
+    id("org.jetbrains.kotlin.plugin.serialization") version "2.0.0" apply false
+    id("com.google.dagger.hilt.android") version "2.57.2" apply false
+    id("com.google.devtools.ksp") version "2.3.1" apply false
+}
